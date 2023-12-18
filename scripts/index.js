02811148b2e367bb817a3144efe0f0ee05ff2da4
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2023-12-18 18:39:49 +0530

    Improve WhatsApp link and add other improvements

--- scripts/index.js
+++ scripts/index.js
@@ -19,6 +19,13 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
+
+function redirectToWhatsApp() {
+    window.open('https://wa.me/918076199461?text=Hello%20Educine%20Academy!%20%F0%9F%91%8B%20I\'m%20interested%20in%20learning%20more%20about%20your%20coaching%20classes%20and%20courses.%20Can%20you%20provide%20me%20with%20more%20information?%20%F0%9F%93%9A', '_blank');
+}
+
+
+
 let courseValue = "";
 const enrollBtns = document.getElementsByClassName("course-btn");
 const btnArray = Array.from(enrollBtns);
