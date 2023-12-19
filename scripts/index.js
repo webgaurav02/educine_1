9f36e51f6db87e3187fc37610000e875ee147680
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2023-12-19 15:38:42 +0530

    Add section for Demo Class

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,10 @@ function redirectToWhatsApp() {
     window.open('https://wa.me/918076199461?text=Hello%20Educine%20Academy!%20%F0%9F%91%8B%20I\'m%20interested%20in%20learning%20more%20about%20your%20coaching%20classes%20and%20courses.%20Can%20you%20provide%20me%20with%20more%20information?%20%F0%9F%93%9A', '_blank');
 }
 
+function bookOnWhatsApp() {
+    window.open('https://wa.me/918076199461?text=Hello%20Educine%20Academy!%20%F0%9F%91%8B%20I%20would%20like%20to%20book%20a%20free%20demo%20class%20with%20you.%20Can%20you%20provide%20me%20with%20more%20information?%20%F0%9F%93%9A', '_blank');
+}
+
 
 
 let courseValue = "";
