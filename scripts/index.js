820619b0cec037465ab75ef9aabe9d29035f9568
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2024-01-06 14:56:09 +0530

    Create Syllabus page

--- scripts/index.js
+++ scripts/index.js
@@ -20,6 +20,55 @@ const swiper = new Swiper('.swiper', {
 });
 
 
+
+
+// Typing animation for title
+const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const phrases = ["Sainik School", "RMS", "RIMC", "NDA"];
+  const el = document.getElementById("type-sp");
+
+  let sleepTime = 80;
+
+  let curPhraseIndex = 0;
+
+  const writeLoop = async () => {
+    while (true) {
+      let curWord = phrases[curPhraseIndex];
+
+      for (let i = 0; i < curWord.length; i++) {
+        el.innerText = curWord.substring(0, i + 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 10);
+
+      for (let i = curWord.length; i > 0; i--) {
+        el.innerText = curWord.substring(0, i - 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 5);
+
+      if (curPhraseIndex === phrases.length - 1) {
+        curPhraseIndex = 0;
+      } else {
+        curPhraseIndex++;
+      }
+    }
+  };
+
+  writeLoop();
+
+
+
+
+
+
+
+
 function redirectToWhatsApp() {
     window.open('https://wa.me/918292475069?text=Hello%20Educine%20Academy!%20%F0%9F%91%8B%20I\'m%20interested%20in%20learning%20more%20about%20your%20coaching%20classes%20and%20courses.%20Can%20you%20provide%20me%20with%20more%20information?%20%F0%9F%93%9A', '_blank');
 }
@@ -43,25 +92,3 @@ btnArray.forEach((button) => {
         courseValue = "";
     });
 });
-
-
-// View full syllabus
-const syllabusBtn = document.getElementsByClassName("syllabus-link")[0];
-const hideBtn = document.getElementsByClassName("hide-syll")[0];
-hideBtn.style.display = "none";
-const fullSyllabus = document.getElementsByClassName("full-syll");
-syllabusBtn.addEventListener('click', () => {
-    Array.from(fullSyllabus).forEach( (syl) => {
-        syl.style.display = "block";
-    })
-    syllabusBtn.style.display = "none";
-    hideBtn.style.display = "block";
-})
-
-hideBtn.addEventListener('click', () => {
-    Array.from(fullSyllabus).forEach( (syl) => {
-        syl.style.display = "none";
-    })
-    syllabusBtn.style.display = "block";
-    hideBtn.style.display = "none";
-})
