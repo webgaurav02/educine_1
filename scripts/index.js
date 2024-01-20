727a8714ebe8e20ca21ee75f0d13bd3d0567f614
“webgaurav02” <gauravcodes123@gmail.com>
2024-01-20 13:47:29 +0530

    Add call to phone button

--- scripts/index.js
+++ scripts/index.js
@@ -93,7 +93,9 @@ writeLoop();
 
 
 
-
+function placeCall() {
+  window.open('tel:8076199461');
+}
 
 function redirectToWhatsApp() {
   window.open('https://wa.me/918292475069?text=Hello%20Educine%20Academy!%20%F0%9F%91%8B%20I\'m%20interested%20in%20learning%20more%20about%20your%20coaching%20classes%20and%20courses.%20Can%20you%20provide%20me%20with%20more%20information?%20%F0%9F%93%9A', '_blank');
